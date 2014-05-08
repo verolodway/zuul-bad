b00bef3684f7938544ed38412f5e57857f7bb44f
Miguel Bayon <miguel.bayon@iessanandres.com>
2014-05-08 21:24:40 +0200

    Modificada la forma de asignar ids a los items

--- Item.java
+++ Item.java
@@ -10,6 +10,7 @@ public class Item
     private String description;
     private double weight;
     private int id;
+    private static int siguienteId = 1;
     
     /**
      * Constructor for objects of class Item
@@ -17,9 +18,10 @@ public class Item
      * @param description The item's description
      * @param weight The item's weight
      */
-    public Item(int id, String description, double weight)
+    public Item(String description, double weight)
     {
-        this.id = id;
+        this.id = siguienteId;
+        siguienteId++;
         this.description = description;
         this.weight = weight;
     }

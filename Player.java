819a703811e9c5b0d697fe233751631dc8b5f914
Miguel Bayon <miguel.bayon@iessanandres.com>
2014-05-08 20:40:11 +0200

    Añadida la clase Player vacia

---
+++ Player.java
@@ -0,0 +1,20 @@
+
+/**
+ * A player of the game
+ * 
+ * @author Miguel Bayon 
+ * @version 1.0
+ */
+public class Player
+{
+
+    /**
+     * Constructor for objects of class Player
+     */
+    public Player()
+    {
+
+    }
+
+
+}

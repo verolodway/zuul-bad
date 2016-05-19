d674efe2936d316a44416684b238e5db0c823ed0
Verónica <verosgfc11@gmail.com>
2016-05-19 23:31:11 +0200

    Terminda actividad 127

---
+++ WorldOfZuul.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class WorldOfZuul here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class WorldOfZuul
+{
+    private static Game JUEGO = new Game();
+    
+    public static void main(String[] args){
+        JUEGO.play();
+    }
+}
